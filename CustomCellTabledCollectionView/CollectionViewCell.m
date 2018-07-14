aab98bb3cbd5cdfa08cb8828db0a825591dbc065
Varun Chopra <vnchopra@gmail.com>
2018-07-13 21:31:01 -0700

    Added code to collectionview cell.
    Added code to show how collectionview cell can be updated

--- CustomCellTabledCollectionView/CollectionViewCell.m
+++ CustomCellTabledCollectionView/CollectionViewCell.m
@@ -9,5 +9,28 @@
 #import "CollectionViewCell.h"
 
 @implementation CollectionViewCell
+  
+//Here can you add custom stuff to your cell if you like
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setupCustomStuff];
+    }
+    return self;
+}
+
+- (void)setFrame:(CGRect)frame
+{
+    [super setFrame:frame];
+    [self setNeedsDisplay]; // force drawRect:
+}
+
+#pragma mark - Create Subviews
+
+- (void)setupCustomStuff
+{
+    //Add custom stuff here
+}
 
 @end
